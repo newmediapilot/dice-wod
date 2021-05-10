efdf66103c4f72c48970f72aa5a0dd86b7653ee5
newmediapilot <marcin@newmediapilot.com>
2021-05-10 01:24:37 -0400

    feature(): initialize firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
-import { APP_INITIALIZER, ErrorHandler, NgModule } from "@angular/core";
-import { Router } from "@angular/router";
+import {APP_INITIALIZER, ErrorHandler, NgModule} from "@angular/core";
+import {Router} from "@angular/router";
 import * as Sentry from "@sentry/angular";
+import {AngularFireModule} from '@angular/fire';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -9,11 +10,14 @@ import {StartComponent} from './components/start/start.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CustomizeComponent} from './components/customize/customize.component';
 import {WodConfigService} from './services/wod-config.service';
-import { IndexComponent } from './components/index/index.component';
-import { TimerComponent } from './components/common/timer/timer.component';
-import { WorkoutTimeComponent } from './components/workout-time/workout-time.component';
-import { WorkoutRoundsComponent } from './components/workout-rounds/workout-rounds.component';
-import { SummaryComponent } from './components/summary/summary.component';
+import {IndexComponent} from './components/index/index.component';
+import {TimerComponent} from './components/common/timer/timer.component';
+import {WorkoutTimeComponent} from './components/workout-time/workout-time.component';
+import {WorkoutRoundsComponent} from './components/workout-rounds/workout-rounds.component';
+import {SummaryComponent} from './components/summary/summary.component';
+import {environment} from '../environments/environment';
+import {AngularFireAnalyticsModule} from '@angular/fire/analytics';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,9 @@ import { SummaryComponent } from './components/summary/summary.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
@@ -47,7 +54,8 @@ import { SummaryComponent } from './components/summary/summary.component';
     },
     {
       provide: APP_INITIALIZER,
-      useFactory: () => () => {},
+      useFactory: () => () => {
+      },
       deps: [Sentry.TraceService],
       multi: true,
     },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDdg6i2v9cjB1AdH9ZsMuD9FjxDzEQ1f2g",
+    authDomain: "dice-wod.firebaseapp.com",
+    projectId: "dice-wod",
+    storageBucket: "dice-wod.appspot.com",
+    messagingSenderId: "507497705640",
+    appId: "1:507497705640:web:fab27f00c8d4b54674d29d",
+    measurementId: "G-FES08PL6SZ"
+  }
 };
 
 /*
